e9747daab2b03228b5aa60b085996cc2a67c3120
codingobviously <codingobviously@gmail.com>
2011-07-12 01:57:28 -0700

    Sort of improved variable name and comments

--- snippets/find_lightest_imagefield.php
+++ snippets/find_lightest_imagefield.php
@@ -6,19 +6,19 @@
 module_load_include('inc', 'content', 'includes/content.node_form');
 $content_types = array('page', 'story', 'product', 'some_content_type');
 
-$imagefields_per_content_type = array();
+$lightest_imagefields = array(); // arranged by content type
 foreach ($content_types as $content_type_name) {
   $content_type_data = content_types($content_type_name);
   $last_weight = NULL;
   foreach ($content_type_data['fields'] as $field_name => $field_data) {
     if ($field_data['widget']['type'] == 'imagefield_widget' && (is_null($last_weight) || (int)$field_data['widget']['weight'] < $last_weight)) {
-        $imagefields_per_content_type[$content_type_name] = $field_name;
+        $lightest_imagefields[$content_type_name] = $field_name;
         $last_weight = (int)$field_data['widget']['weight'];
     }
   }
 }
-/** Example Usage:
+/** Hypothetical Usage:
  * $node = load_some_node_i_want();
- * $node->$imagefields_per_content_type[$node->type]; // Accessing the lightest imagefield.
+ * $node->$lightest_imagefields[$node->type]; // Accessing the lightest imagefield of this node's content type.
  */
 
